3be700fe7f2baf7790733a3f1b8f016fd9a43d02
Mades Eladrian <mades.eladrian@gmail.com>
2023-06-23 15:41:07 -0300

    ensure GetCategory receive correct params

---
+++ FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInput.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+
+public class GetCategoryInput : IRequest<GetCategoryOutput>
+{
+    public Guid Id { get; set; }
+
+    public GetCategoryInput(Guid id) => Id = id;
+}
